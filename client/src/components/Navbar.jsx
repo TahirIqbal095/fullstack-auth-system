bbd9f2c23bf428ba8463c821ab7c27dc54e1d658
Tahir Iqbal <shahtahir786@gmail.com>
2024-10-10 11:16:51 +0530

    added shared layout

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+    return (
+        <nav style={{ textAlign: "center", padding: "0rem 4rem" }}>
+            <h3>Full Stack Login System</h3>
+        </nav>
+    );
+}
+
+export default Navbar;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,14 +6,17 @@ import "./index.css";
 import ProfilePage from "./pages/ProfilePage";
 import HomePage from "./pages/HomePage";
 import Register from "./pages/Register";
+import SharedLayout from "./components/SharedLayout";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/me" element={<ProfilePage />} />
-                <Route path="/register" element={<Register />} />
+                <Route path="/" element={<SharedLayout />}>
+                    <Route index element={<HomePage />} />
+                    <Route path="/me" element={<ProfilePage />} />
+                    <Route path="/register" element={<Register />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     </StrictMode>

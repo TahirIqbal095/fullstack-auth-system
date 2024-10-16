35ea6e132178702ce9915b205473450f90629eac
Tahir Iqbal <shahtahir786@gmail.com>
2024-10-16 17:39:44 +0530

    removed req interceptor

--- client/src/hooks/useAxiosPrivate.jsx
+++ client/src/hooks/useAxiosPrivate.jsx
@@ -3,20 +3,9 @@ import useRefresh from "./useRefresh";
 import { axiosPrivate } from "../api/axios";
 
 const useAxiosPrivate = () => {
-    const accessToken = localStorage.getItem("accessToken");
     const refreshToken = useRefresh();
 
     useEffect(() => {
-        const requestInterceptor = axiosPrivate.interceptors.request.use(
-            (config) => {
-                if (!config.headers["Authorization"]) {
-                    config.headers["Authorization"] = `Bearer ${accessToken}`;
-                }
-                return config;
-            },
-            (error) => Promise.reject(error)
-        );
-
         const responseInterceptor = axiosPrivate.interceptors.response.use(
             (response) => response,
 
@@ -24,22 +13,14 @@ const useAxiosPrivate = () => {
                 const prevReq = error?.config;
                 if ((error.response?.status === 401) & !prevReq.sent) {
                     prevReq.sent = true;
-                    const newAccessToken = await refreshToken();
-
-                    if (newAccessToken) {
-                        prevReq.headers[
-                            "Authorization"
-                        ] = `Bearer ${newAccessToken}`;
-                    }
+                    await refreshToken();
                     return axiosPrivate(prevReq);
                 }
             },
-
             (error) => Promise.reject(error)
         );
 
         return () => {
-            axiosPrivate.interceptors.request.eject(requestInterceptor);
             axiosPrivate.interceptors.response.eject(responseInterceptor);
         };
     }, [accessToken]);

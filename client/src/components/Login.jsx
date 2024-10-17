26c729ccd942aa29e4596ce7d5e7d0b77bf6c3f9
Tahir Iqbal <shahtahir786@gmail.com>
2024-10-17 12:59:06 +0530

    login page style

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -4,16 +4,28 @@ import { useNavigate } from "react-router-dom";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-function Login() {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const navigate = useNavigate();
+import { Button } from "@/components/ui/button";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { ReloadIcon } from "@radix-ui/react-icons";
 
-    const inputClasses =
-        "w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow";
+function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setLoading(true);
 
         try {
             const response = await fetch(`${API_URL}/login`, {
@@ -24,7 +36,7 @@ function Login() {
                 },
 
                 body: JSON.stringify({
-                    email: username,
+                    email: email,
                     password: password,
                 }),
             });
@@ -39,43 +51,53 @@ function Login() {
             }
         } catch (error) {
             console.log("error while login ", error);
+        } finally {
+            setLoading(false);
         }
     };
-
     return (
-        <div className="max-w-3xl p-4">
-            <h1 className="text-2xl font-bold text-gray-600">Log in</h1>
-
-            <form action="post" className="space-y-3 mt-6">
-                <div>
-                    <input
-                        type="email"
-                        name="email"
-                        required
-                        onChange={(e) => setUsername(e.target.value)}
-                        className={inputClasses}
-                        placeholder="Enter Email"
-                    />
-                </div>
-                <div>
-                    <input
-                        type="password"
-                        name="password"
-                        required
-                        onChange={(e) => setPassword(e.target.value)}
-                        className={inputClasses}
-                        placeholder="Enter password"
-                    />
-                </div>
-
-                <button
-                    onClick={handleSubmit}
-                    className="mt-4 bg-gray-800 text-white px-5 py-2 rounded-md hover:bg-gray-900 text-sm"
-                >
-                    Login
-                </button>
-            </form>
-        </div>
+        <Card className="max-w-[350px]">
+            <CardHeader>
+                <CardTitle className="text-lg">Log in</CardTitle>
+                <CardDescription>
+                    Enter your correct credentials to log in
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <form>
+                    <div className="grid w-full items-center gap-4">
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor="email">Email</Label>
+                            <Input
+                                id="email"
+                                type="email"
+                                placeholder="Enter your email"
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </div>
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor="password">Password</Label>
+                            <Input
+                                id="password"
+                                placeholder="Enter you password"
+                                type="password"
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+                    </div>
+                </form>
+            </CardContent>
+            <CardFooter className="flex justify-end">
+                {loading ? (
+                    <Button disabled>
+                        <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+                        Login
+                    </Button>
+                ) : (
+                    <Button onClick={handleSubmit}>Login</Button>
+                )}
+            </CardFooter>
+        </Card>
     );
 }
 
